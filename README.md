6d27c54efb71d4b4d1e8828f3a1751b765647f0d
John Häggerud <john.haggerud@lnu.se>
2014-09-23 08:37:52 +0200

    Added readme and ignore for pushing it to github

---
+++ README.md
@@ -0,0 +1,1 @@
+## This is a assignment for Webbteknik I
---
+++ server/spec/fileReaderSpec.spec.js
@@ -0,0 +1,10 @@
+
+try {
+describe("FileReader", function() {
+	it("Should make noice", function() {
+		var i = 10;
+		expect(i).toBe(10);
+	});
+});
+}
+catch(er){console.log(er);}
