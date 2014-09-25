d19752888f5c1339c7c20f61ba2df65a03c035f8
John Häggerud <john.haggerud@lnu.se>
2014-09-25 15:52:41 +0200

    Added some test to the questHandler module. Try to write the module with event handling

--- server/spec/questHandler.spec.js
+++ server/spec/questHandler.spec.js
@@ -10,9 +10,17 @@ describe("Check the creation phase", function() {
 });
 
 
-describe("Check that we get the fetched data\n", function() {
+describe("Check that we can get data\n", function() {
 	var onData = false;
 	var obj = require('../lib/questHandler.js');
+	//var o = obj.createQuestHandler();
+
+
+	it("Should throw an exception telling that we havn´t got any data yet", function(){
+		var o = obj.createQuestHandler();
+		expect(o.getQuestion(1)).toThrow("Error: Don´t have any data yet");
+	});
+
 	// before the test we set upp the call
 	beforeEach(function() {
 		var o = obj.createQuestHandler();
@@ -28,7 +36,30 @@ describe("Check that we get the fetched data\n", function() {
 
 
 	});
-	it("Should get the event 'onData'", function() {
+	it("Should get the event onData", function() {
 		expect(onData).toBeTruthy();
 	});
+
+	it("Should return the number of Questions", function() {
+		var o = obj.createQuestHandler();
+		var nr = o.getNumberOfQuestions();
+		expect(nr).toBe(typeof "number");
+		expect(nr > 0).toBeTruthy();
+	});
+
+	it("Should get an JSON object containing a Question and an Answer", function(){
+		var o = obj.createQuestHandler();
+		if(o.getNumberOfQuestions() > 0) {
+			var data = o.getQuestion(1);
+			expect(data).toBeDefinied();
+		}
+		
+
+	});
+
+	it("Should throw an error indicating we trying to get an index outside the array", function(){
+		var o = obj.createQuestHandler();
+		expect(o.getQuestion(o.getNumberOfQuestions() + 1)).toThrow("Error: Don´t have any data yet");
+	});
 });
+
