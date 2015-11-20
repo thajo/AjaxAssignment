b722651e4eeba047d8ab50ad5f24af6c42c6aff8
John Häggerud <john.haggerud@lnu.se>
2015-11-20 14:02:02 +0100

    updated

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var app = require("./lib/server.js");
-app.startApp(function(){
-    console.log("Got callback");
+
+app.startApp(function() {
+    console.log("Server app i running...");
 });
